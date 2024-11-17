3a9775c0db56b9082ce62a84780086f076fd70e1
vijaypvk <vijaypvk001@gmail.com>
2024-11-17 15:04:25 +0530

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,9 @@
 function Footer (){
     return (
         <footer className="sticky bottom-0 shadow-2xl py-10 px-4 bg-gray-200">
-            
+            <div className="justify-start">
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">next</button>
+            </div>
         </footer>
     )
 }
