5e0f574c4c94e6fcdcfe7c918e164ce1c7fbe74a
Stellar2703 <rajammalshash@gmail.com>
2024-11-23 23:20:36 +0530

    imporving code readability

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,37 +1,3 @@
-// import React from "react";
-
-// function Footer() {
-//   return (
-//     <footer className="sticky bottom shadow-2xl py-7 px-8 bg-gray-300 text-white flex items-center justify-between">
-    
-//       <div>
-//         <button
-//           type="button"
-//           className="bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-7 py-2.5 focus:outline-none"
-//         >
-//           Finish
-//         </button>
-//       </div>
-
-//       <div className="text-center text-black font-semibold">
-//         Student ID: 7376231CS345
-//       </div>
-
-//       {/* Right Section: Skip and Submit Buttons */}
-//       <div className="flex items-center space-x-4">
-//         {/* <button
-//           type="button"
-//           className="bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-gray-500 font-medium rounded-lg text-sm px-7 py-2.5 focus:outline-none"
-//         >
-//           Next
-//         </button> */}
-//       </div>
-//     </footer>
-//   );
-// }
-
-// export default Footer;
-
 import React from "react";
 
 function Footer() {
